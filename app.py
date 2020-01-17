569ea28280ab888b9a65876f6878a5253c0a0617
Rajmohanraj <mohanraj.thangavel.a@gmail.com>
2020-01-17 14:35:59 +0530

    varibles

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
-#first python program
-print ("hello world")
+#varibles 
+student_count = 100 # integer varibles
+pass_percent = 78.8 # floting varibles
+result_published = True # boolean
+coures_name = ("python") # string 
